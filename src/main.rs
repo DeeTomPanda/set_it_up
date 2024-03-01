079ed817afc103dc0cbb88553d49229f6888948c
Dtompanda <thewalkerdarkknight@gmail.com>
2024-03-01 23:40:22 +0530

    Working React setup done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::io::Read;
 use std::{fs,env,io};
-use std::process::Command;
+use std::process::{Command,Stdio};
+use std::io::Write;
 
 
 fn main() {
@@ -17,14 +19,28 @@ fn main() {
 
     // Check if any project with same name exists
     if !(project_path).exists(){
-        react_setup(project_name);
+        react_setup(project_name,false);
     }
 
 }
 
-fn react_setup(project_name:&str){
-    let op=Command::new("yarn").args(["create","vite"]).output().expect("Failed for some reason");
-    print!("{:?}",&op);
+fn react_setup(project_name:&str,TS:bool){
+    let mut op=Command::new("sh").args(&["-c",&format!("yarn create vite {} --template react",project_name)])
+    .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("Failed to start command");
+    
+
+    let status=op.wait().expect("Encountered error while running command");
+
+
+    // Check the exit status of the command
+    if status.success() {
+        println!("Command executed successfully");
+    } else {
+        eprintln!("Command failed with: {:?}", status);
+    }    
 }
 
 fn angular_setup(){}
